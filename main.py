2856440b1cd249e454e791949cd48cbbbbe0a160
Eduardo Daniel Martinez <eduardo.d.martinez@outlook.com>
2026-08-13 20:21:59 -0300

    Refactorizado - creada función procesar_imagen

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import base64
 import tempfile
 
-from fastapi import FastAPI, File, HTTPException, UploadFile
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from ocr import ocr
 from groq_a_json import crear_json
-import json
-import asyncio
+
+from procesar_imagen import procesar_imagen
 
 app = FastAPI()
 
@@ -17,42 +16,11 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-ALLOWED_TYPES = {"image/jpeg", "application/pdf"}
-MAX_SIZE_MB = 20
-EXTENSIONES = {
-    "image/jpeg": ".jpg",
-    "application/pdf": ".pdf",
-}
-
 @app.post("/ocr")
 async def root(file: UploadFile = File(...)):
-    if file.content_type not in ALLOWED_TYPES:
-        raise HTTPException(
-            status_code=400,
-            detail=f"Tipo de archivo no soportado: {file.content_type}"
-        )
-    contenido = await file.read()
-    if len(contenido) > MAX_SIZE_MB * 1024 * 1024:
-        raise HTTPException(
-            status_code=400,
-            detail=f"El archivo excede el tamaño máximo permitido de {MAX_SIZE_MB} MB"
-        )
-
-    # Guardar el archivo temporalmente
-    extension = EXTENSIONES[file.content_type]
-    tmp_path = None
     
-    try:
-        with tempfile.NamedTemporaryFile(delete=False, suffix=extension) as tmp_file:
-            tmp_file.write(contenido)
-            tmp_path = tmp_file.name
-
-        # Realizar OCR en la imagen del ticket
-        texto_ocr = await ocr(tmp_path)
-    finally:
-        if tmp_path:
-            import os
-            os.remove(tmp_path)
+    # Procesar la imagen y obtener el texto OCR
+    texto_ocr = await procesar_imagen(file)
 
     # Enviar el texto OCR a la función de Groq para obtener el JSON estructurado
     resultado_json = await crear_json(texto_ocr)
